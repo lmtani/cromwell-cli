1e1ba2f3d1455bc97a4c48bc989c094bef54d5f0
Lucas Taniguti <lucas.biotec07@gmail.com>
2021-02-01 23:11:31 +0000

    delivery cli

--- app/main.go
+++ app/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	_cromwellDelivery "github.com/lmtani/cromwell-cli/cromwell/delivery/cli"
 	_cromwellRepo "github.com/lmtani/cromwell-cli/cromwell/repository/http"
 	_cromwellUsecase "github.com/lmtani/cromwell-cli/cromwell/usecase"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	app := &cli.App{
+		Name:  "cromwell-cli",
+		Usage: "Command line interface for Cromwell Server",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "token",
+				Aliases:  []string{"t"},
+				Required: false,
+				Usage:    "Bearer token to be included in HTTP requsts",
+			},
+			&cli.StringFlag{
+				Name:  "host",
+				Value: "http://127.0.0.1:8000",
+				Usage: "Url for your Cromwell Server",
+			},
+		},
+		Commands: []*cli.Command{},
+	}
+
 	cromwellRepo := _cromwellRepo.NewHTTPCromwellRepository("http://127.0.0.1:8000", "")
 	cromwellUsecase := _cromwellUsecase.NewCromwellUsecase(cromwellRepo)
-	_cromwellDelivery.NewCromwellHandler(cromwellUsecase)
+	_cromwellDelivery.NewCromwellHandler(app, cromwellUsecase)
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println("cromwell.command.error", err)
+	}
 }
--- cromwell/delivery/cli/handler.go
+++ cromwell/delivery/cli/handler.go
@@ -1,9 +1,6 @@
 package cli
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/lmtani/cromwell-cli/domain"
 	"github.com/urfave/cli/v2"
 )
@@ -12,44 +9,20 @@ type CromwellHandler struct {
 	CromwellUsecase domain.CromwellUsecase
 }
 
-func NewCromwellHandler(us domain.CromwellUsecase) {
+func NewCromwellHandler(app *cli.App, us domain.CromwellUsecase) {
 	handler := CromwellHandler{
 		CromwellUsecase: us,
 	}
 
-	app := &cli.App{
-		Name:  "cromwell-cli",
-		Usage: "Command line interface for Cromwell Server",
+	app.Commands = []*cli.Command{{
+		Name:    "query",
+		Aliases: []string{"q"},
+		Usage:   "Query workflows",
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "token",
-				Aliases:  []string{"t"},
-				Required: false,
-				Usage:    "Bearer token to be included in HTTP requsts",
-			},
-			&cli.StringFlag{
-				Name:  "host",
-				Value: "http://127.0.0.1:8000",
-				Usage: "Url for your Cromwell Server",
-			},
+			&cli.StringFlag{Name: "name", Aliases: []string{"n"}, Required: false},
 		},
-		Commands: []*cli.Command{
-			{
-				Name:    "query",
-				Aliases: []string{"q"},
-				Usage:   "Query workflows",
-				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "name", Aliases: []string{"n"}, Required: false},
-				},
-				Action: handler.QueryWorkflow,
-			},
-		},
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		fmt.Println("cromwell.command.error: ", err)
-	}
+		Action: handler.QueryWorkflow,
+	}}
 }
 
 func (h *CromwellHandler) QueryWorkflow(c *cli.Context) error {
